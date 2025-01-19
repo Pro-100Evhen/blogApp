0d1943135636033dc4b9e07af70131f68326f7e3
Evhen Nazarenko <Evgen1215@ukr.net>
2025-01-19 09:38:36 +0200

    Add: post author to posts

--- src/components/Posts/PostItem.js
+++ src/components/Posts/PostItem.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { selectUserById } from "../../store/slices/usersSlice";
 
 const PostItem = ({ id, title, body }) => {
+   const postAuthor = useSelector((state) => selectUserById(state, id));
    return (
       <div className="border border-white rounded-xl p-3 text-white" key={id}>
          <h3 className="text-2xl mb-2 capitalize">{title}</h3>
          <p className="mb-2">{body}</p>
+         {postAuthor ? (
+            <p className="mb-2">Author: {postAuthor.username}</p>
+         ) : (
+            <p className="mb-2">Author: user</p>
+         )}
+
          <Link className="underline hover:no-underline" to={`/post/${id}`}>
             Read more
          </Link>
--- src/components/Posts/SinglePost.js
+++ src/components/Posts/SinglePost.js
@@ -2,10 +2,12 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { selectPostById } from "../../store/slices/postsSlice";
+import { selectUserById } from "../../store/slices/usersSlice";
 
 export const SinglePost = () => {
    const { postId } = useParams();
    const post = useSelector((state) => selectPostById(state, postId));
+   const postAuthor = useSelector((state) => selectUserById(state, postId));
 
    if (!post) {
       return <p>Post not found!</p>;
@@ -15,6 +17,7 @@ export const SinglePost = () => {
       <div className="mx-auto max-w-screen-xl  py-7  h-[calc(100vh-60px)] text-white  flex flex-col">
          <h3 className="text-4xl mb-5">Post title: {post.title}</h3>
          <p className="text-xl">Post content: {post.body}</p>
+         {postAuthor ? <p>Author: {postAuthor.name}</p> : <p>Author: user</p>}
       </div>
    );
 };
